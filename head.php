46ba5df964622bd8193e6d88fe9f4a11086ea2f1
K S <ksglobalscbe@gmail.com>
2025-05-20 17:23:55 +0530

    change 1

--- head.php
+++ head.php
@@ -1,7 +1,9 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>vijay ramya</title>
+    <title>vijay ramya krupa
+
+    </title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
